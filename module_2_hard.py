072c602cea89a981e146f92b6ae5288e37a74bb2
GrayThing <samorodov-artem@mail.ru>
2024-08-13 10:30:26 +0300

    Минорные доработки в названиях переменных 2023/10/02 00:00|Дополнительное практическое задание по модулю*

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,16 +1,16 @@
 import random
 
-first_plate = random.choice(range(3, 21))
+n = random.choice(range(3, 21))
 
 
 def get_password(number):
-    password = ''
+    result = ''
     for number_first in range(1, 21):
         for number_second in range(1, 21):
-            if number_first < number_second and first_plate % (number_first + number_second) == 0:
-                password = password + str(number_first) + str(number_second)
-    return password
+            if number_first < number_second and n % (number_first + number_second) == 0:
+                result = result + str(number_first) + str(number_second)
+    return result
 
 
-print(f'Password for the first plate with number {first_plate} is {get_password(first_plate)}')
+print(f'Password for the first plate with number {n} is {get_password(n)}')
 
